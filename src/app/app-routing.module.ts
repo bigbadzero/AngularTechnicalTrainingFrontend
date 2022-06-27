22645b09efb6dea49fa5f9c328703718f1a42070
adam watson <adamwatson08@gmail.com>
2022-06-27 13:13:45 -0500

    added icon to sideNav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {EmployeeComponent} from './employee/employee.component'
-import {EmployeeAssetsComponent} from './employee-assets/employee-assets.component';
-import {AssetDetailsComponent} from '../app/asset-details/asset-details.component'
+import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeAssetsComponent } from './employee-assets/employee-assets.component';
+import { AssetDetailsComponent } from '../app/asset-details/asset-details.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: '/home',
-    pathMatch: 'full'
-},
+    pathMatch: 'full',
+  },
   {
-    path:"home",
-    component: HomeComponent
+    path: 'home',
+    component: HomeComponent,
   },
-  { 
-    path:"assets/employee/:id",
-    component: EmployeeAssetsComponent
+  {
+    path: 'assets/employee/:id',
+    component: EmployeeAssetsComponent,
   },
-  { 
-    path:"assetDetails/:id",
-    component: AssetDetailsComponent
-  }
+  {
+    path: 'assetDetails/:id',
+    component: AssetDetailsComponent,
+  },
+  { path: 'employees', 
+    component: EmployeesComponent 
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
