bef4442d8666106cef9754d82c75b418d732c713
adam watson <adamwatson08@gmail.com>
2022-06-27 23:47:08 -0500

    added logging to add-asset-dialog

--- src/app/components/dialogs/Asset/asset-add-dialog/asset-add-dialog.component.ts
+++ src/app/components/dialogs/Asset/asset-add-dialog/asset-add-dialog.component.ts
@@ -4,7 +4,7 @@ import { AssetService } from '../../../../shared/services/assetService';
 import { NgForm } from '@angular/forms';
 import { AssetDialogData } from '../../../../shared/models/assetDialogData';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-
+import { NGXLogger } from 'ngx-logger';
 @Component({
   selector: 'app-asset-add-dialog',
   templateUrl: './asset-add-dialog.component.html',
@@ -14,20 +14,23 @@ export class AssetAddDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<AssetAddDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: AssetDialogData,
-    public assetService: AssetService
+    public assetService: AssetService,
+    private logger: NGXLogger
   ) {}
 
   ngOnInit(): void {
-    console.log(this.data);
+    this.logger.trace('initializing asset-add-dialog component')
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    this.logger.trace('submitting form to add asset')
     let employeeId:number;
     if(this.data.lockEmployee == true){
+      this.logger.trace('employee dropdown is locked')
       employeeId = this.data.asset.employeeId;
     }
     else{
+      this.logger.trace('employee dropdown is not locked')
       employeeId = form.value.employeeId;
     }
     const asset: Asset = {
@@ -36,21 +39,23 @@ export class AssetAddDialogComponent {
       employeeId: employeeId,
     };
 
+    this.logger.trace('adding asset');
     this.assetService.addAsset(asset).subscribe(
       (result) => {
         this.dialogRef.close();
       },
       (err) => {
-        console.log(err);
+        this.logger.error(err);
       },
       () => {
-        console.log('record added');
+        this.logger.trace('record added');
         window.location.reload();
       }
     );
   }
 
   onNoClick(): void {
+    this.logger.trace('closing AssetAddDialogComponent')
     this.dialogRef.close();
   }
 }
