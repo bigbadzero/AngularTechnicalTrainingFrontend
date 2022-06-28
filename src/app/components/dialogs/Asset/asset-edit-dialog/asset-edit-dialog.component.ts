16fcaa145bb2534782cb99f19e0a4583ee90b189
adam watson <adamwatson08@gmail.com>
2022-06-27 23:50:48 -0500

    added logging to asset-edit-dialog component

--- src/app/components/dialogs/Asset/asset-edit-dialog/asset-edit-dialog.component.ts
+++ src/app/components/dialogs/Asset/asset-edit-dialog/asset-edit-dialog.component.ts
@@ -4,6 +4,7 @@ import { AssetService } from '../../../../shared/services/assetService';
 import { NgForm } from '@angular/forms';
 import { AssetDialogData } from '../../../../shared/models/assetDialogData';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NGXLogger } from 'ngx-logger';
 @Component({
   selector: 'app-asset-edit-dialog',
   templateUrl: './asset-edit-dialog.component.html',
@@ -13,19 +14,24 @@ export class AssetEditDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<AssetEditDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: AssetDialogData,
-    public assetService: AssetService
+    public assetService: AssetService,
+    private logger: NGXLogger
   ) {}
 
-  onInit() {}
+  onInit() {
+    this.logger.trace('initializing asset-edit-dialog component')
+  }
 
   onSubmit(form: NgForm) {
+    this.logger.trace('submitting form to edit asset')
     let retired: boolean ;
     if (form.value.retired == true) {
+      this.logger.trace('retired is checked')
       retired = true;
     } else {
+      this.logger.trace('retired is not checked')
       retired = false;
     }
-    console.log(form.value);
     const asset: Asset = {
       tagId: form.value.tagId,
       assetTypeId: form.value.assetTypeId,
@@ -35,21 +41,23 @@ export class AssetEditDialogComponent {
       retired: retired,
       dateRetired: form.value.dateRetired,
     };
+    this.logger.trace('editing asset');
     this.assetService.updateAsset(asset).subscribe(
       (result) => {
         this.dialogRef.close();
       },
       (err) => {
-        console.log(err);
+        this.logger.error(err);
       },
       () => {
-        console.log('record updated');
+        this.logger.trace('record updated');
         window.location.reload();
       }
     );
   }
 
   onNoClick(): void {
+    this.logger.trace('closing AssetEditDialogComponent')
     this.dialogRef.close();
   }
 }
